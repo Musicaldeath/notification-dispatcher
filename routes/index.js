4762e8db5dd21ad567d49e03e1d6397fd9208e99
Alexandre Bouchard <Alexandre Bouchard>
2017-07-02 08:44:48 -0500

    reading headers

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res) {
 router.post('/', ( req, res ) => {
   console.log( req.headers );
   console.log( req.body );
+  console.log( req.SubscribeURL );
 });
 
 module.exports = router;
