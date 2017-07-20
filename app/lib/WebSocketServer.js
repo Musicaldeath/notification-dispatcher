e454b7180e85647829ff4acb6bfff80b078945c6
Alexandre Bouchard <Alexandre Bouchard>
2017-07-20 16:11:01 -0500

    slhsaa2kssojm2

--- app/lib/WebSocketServer.js
+++ app/lib/WebSocketServer.js
@@ -22,31 +22,32 @@ class WebSocketServer {
     this.io = socketIo( server );
     this.io.on('connection', ( socket ) => {
 
-      registerEvent( socket, 'Subscribe', this.subscribeFn );
+      registerEvent( socket, 'Subscribe', this.subscribeFn( this.connections ) );
       registerEvent( socket, 'Unsubscribe', this.unsubscribeFn );
 
     });
   }
 
-  subscribeFn( data ) {
-    console.log( this );
-    console.log( this.connections );
-    var connx = this.connections || [];
-    var conn = getConnection( data.videoId, connx );
-    if( conn === undefined ) connx.push( { videoId: data.videoId, sockets: [ this ] } );
-    else {
-      if( conn.sockets[socket] !== undefined )
-        conn.sockets.push( socket );
-    }
-    console.log( data );
+  subscribeFn( connections ) {
+
+    return function( data ) {
+      var conn = getConnection( data.videoId, connections || [] );
+      if( conn === undefined ) connections.push( { videoId: data.videoId, sockets: [ this ] } );
+      else {
+        if( conn.sockets[this] === undefined )
+          conn.sockets.push( this );
+      }
+      console.log( data );
+    };
   }
 
   unsubscribeFn( data ) {
-    var conn = getConnection( data.videoId, this.connections ||[] );
-    if( conn === undefined ) socket.emit('NoSubscriptions', "No subscriptions for this video");
-    if( conn.sockets[sock] === undefined ) socket.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
+    var sock = this;
+    var conn = getConnection( data.videoId, this.connections || [] );
+    if( conn === undefined ) this.emit('NoSubscriptions', "No subscriptions for this video");
+    if( conn.sockets[sock] === undefined ) this.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
 
-    conn.sockets.splice( socket, 1 );
+    conn.sockets.splice( this, 1 );
     console.log( data );
   }
 
