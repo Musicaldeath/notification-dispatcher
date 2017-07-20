9634ff3189e79769275594e09fb367edeb94c45e
Alexandre Bouchard <Alexandre Bouchard>
2017-07-19 23:58:49 -0500

    syntax 3

--- app/lib/SocketHandler.js
+++ app/lib/SocketHandler.js
@@ -26,7 +26,7 @@ class SocketHandler {
 
       socket.on('unsubscribed', ( data ) => {
         var conn = getConnection( data.videoId );
-        if( conn === undefined ) socket.emit('NoSubscriptions' "No subscriptions for this video");
+        if( conn === undefined ) socket.emit('NoSubscriptions', "No subscriptions for this video");
         if( conn.sockets[sock] === undefined ) socket.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
 
         conn.sockets.splice( socket, 1 );
