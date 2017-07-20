b43f30935ab285bb6238a5e6f86dd5ee86e4089f
Alexandre Bouchard <Alexandre Bouchard>
2017-07-20 08:35:49 -0500

    slhsaa2kssojm2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 
 var server = require('http').createServer( app );
-server.all('/', function( req, res, next) {
+app.all('/', function( req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
