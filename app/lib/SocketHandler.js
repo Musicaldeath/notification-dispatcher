225d9a38229fc3f6fd26d8064f2917dfc789d3fd
Alexandre Bouchard <Alexandre Bouchard>
2017-07-20 00:05:20 -0500

    syntax

--- app/lib/SocketHandler.js
+++ app/lib/SocketHandler.js
@@ -1,39 +1,37 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
+let init = () =>{
+  io.on('connection', ( socket ) => {
+
+    console.log( "Connection to auction websocket feed");
+
+    socket.on('subscribe', ( data ) => {
+      var conn = getConnection( data.videoId );
+      if( conn === undefined ) this.connections.push( { videoId: data.videoId, sockets: [ socket ] } );
+      else {
+        if( conn.sockets[socket] !== undefined )
+          conn.sockets.push( socket );
+      }
+    });
+
+    socket.on('unsubscribed', ( data ) => {
+      var conn = getConnection( data.videoId );
+      if( conn === undefined ) socket.emit('NoSubscriptions', "No subscriptions for this video");
+      if( conn.sockets[sock] === undefined ) socket.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
+
+      conn.sockets.splice( socket, 1 );
+    });
+  });
+
+  server.listen( 3006 );
+}
 
 class SocketHandler {
 
   constructor() {
       this.connections = [];
       init();
-  }
-
-  init() {
-
-    io.on('connection', ( socket ) => {
-
-      console.log( "Connection to auction websocket feed");
-
-      socket.on('subscribe', ( data ) => {
-        var conn = getConnection( data.videoId );
-        if( conn === undefined ) this.connections.push( { videoId: data.videoId, sockets: [ socket ] } );
-        else {
-          if( conn.sockets[socket] !== undefined )
-            conn.sockets.push( socket );
-        }
-      });
-
-      socket.on('unsubscribed', ( data ) => {
-        var conn = getConnection( data.videoId );
-        if( conn === undefined ) socket.emit('NoSubscriptions', "No subscriptions for this video");
-        if( conn.sockets[sock] === undefined ) socket.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
-
-        conn.sockets.splice( socket, 1 );
-      });
-    });
-
-    server.listen( 3006 );
   }
 
   pushMessage( videoId, msg ) {
