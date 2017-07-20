001fe3eec0dce5bb9f5b18485861b62151dac352
Alexandre Bouchard <Alexandre Bouchard>
2017-07-20 14:38:31 -0500

    slhsaa2kssojm2

--- app/lib/WebSocketServer.js
+++ app/lib/WebSocketServer.js
@@ -1,28 +1,23 @@
 var socketIo = require('socket.io');
 
 /*****************************************************/
-let subscribeFn = ( data, getConnection ) => {
-  return function( data ) {
-    var conn = getConnection( data.videoId );
-    if( conn === undefined ) this.connections.push( { videoId: data.videoId, sockets: [ socket ] } );
-    else {
-      if( conn.sockets[socket] !== undefined )
-        conn.sockets.push( socket );
-    }
-    console.log( data );
-  };
+let subscribeFn = ( data ) => {
+  var conn = getConnection( data.videoId );
+  if( conn === undefined ) this.connections.push( { videoId: data.videoId, sockets: [ socket ] } );
+  else {
+    if( conn.sockets[socket] !== undefined )
+      conn.sockets.push( socket );
+  }
+  console.log( data );
 };
 /*****************************************************/
-let unsubscribeFn = ( data, getConnection ) => {
+let unsubscribeFn = ( data ) => {
+  var conn = getConnection( data.videoId );
+  if( conn === undefined ) socket.emit('NoSubscriptions', "No subscriptions for this video");
+  if( conn.sockets[sock] === undefined ) socket.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
 
-  return function( data ) {
-    var conn = getConnectionFor( data.videoId );
-    if( conn === undefined ) socket.emit('NoSubscriptions', "No subscriptions for this video");
-    if( conn.sockets[sock] === undefined ) socket.emit( 'UserNotRegistered',  'User socket not subscribed this notification feed');
-
-    conn.sockets.splice( socket, 1 );
-    console.log( data );
-  };
+  conn.sockets.splice( socket, 1 );
+  console.log( data );
 };
 
 let registerEvent = ( socket, eventName, fn ) => {
@@ -49,16 +44,8 @@ class WebSocketServer {
     this.io = socketIo( server );
     this.io.on('connection', ( socket ) => {
 
-      var subscribeFnBuild = (( data, getConnection )=> {
-        return subscribeFn( data, getConnection );
-      })( getConnection );
-
-      var unsubscribeFnBuild = (( data, getConnection )=>{
-        return unsubscribeFn( data, getConnection );
-      })( getConnection );
-
-      registerEvent( socket, 'Subscribe', subscribeFnBuild );
-      registerEvent( socket, 'Unsubscribe', unsubscribeFnBuild );
+      registerEvent( socket, 'Subscribe', subscribeFn );
+      registerEvent( socket, 'Unsubscribe', unsubscribeFn );
 
     });
 
