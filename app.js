90584805afa9d01c7117d1466b035931282a4491
Alexandre Bouchard <Alexandre Bouchard>
2017-07-20 10:15:59 -0500

    slhsaa2kssojm2

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var contentTypeOverride = require('./utils/contentTypeOverride');
 var app = express();
+var http = require('http');
+var server = http.createServer( app );
+var io = require('socket.io')(server);
+
+server.listen( 3006, function(){
+  console.log('Server started on port 3006');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.use( '/', require('./routes/index'));
 
-
+io.on('connection', function( socket ) {
+  console.log( 'CONNECTED ');
+});
 
 //APP BASIC CONFIG
 app.engine('html', require('ejs').renderFile);
@@ -22,7 +33,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
-app.use(express.static(__dirname + '/public'));
 
 
 //ROUTING MODULES
--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,7 @@ const PubSubController = require('./../app/lib/PubSubController');
 var express = require('express');
 var router  = express.Router();
 
-var app     = express();
-var server = require('http').createServer( app );
-var io = require('socket.io')(server);
-io.on('connection', function(socket) {
-  console.log( 'AAAAAH');
-});
 
-io.set('transports', [            // all transports (optional if you want flashsocket)
-    'websocket'
-    , 'flashsocket'
-    , 'htmlfile'
-    , 'xhr-polling'
-    , 'jsonp-polling'
-]);
-io.set('origins', '*:*');
-server.listen( 3006 );
 /*var socketHandler = new SocketHandler();*/
 router.get('/', ( req, res, next ) =>  {
   res.end();
